de71f0fb3a84272e86b172e560046bdadb8f71ec
Pablo Peña <gmlpubli00@gmail.com>
2022-11-03 12:28:17 -0300

    Feat: handle 404

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const cors = require("cors")
+const path = require("path")
 const PORT = 8080
 
 const app = express()
@@ -11,6 +12,11 @@ app.use(express.static("public", { extensions: ['html'] }))
 
 // Añade rutas
 app.use("/api", require("./routes/"))
+
+// Manejar 404
+app.get('*', function (req, res) {
+  res.status(404).sendFile(path.join(__dirname, "../public/404.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`server listening on port ${PORT}`)
